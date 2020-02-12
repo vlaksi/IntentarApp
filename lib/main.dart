7e6247d5a8f982a2705819e75aa71e9294d73305
vlaksi <daksrapsody@gmail.com>
2020-02-13 00:09:56 +0100

    Scaffold(),fonts & colours

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,36 @@ import 'package:flutter/material.dart';
 
 /*
  Arrow funkcija main, koja ocekuje stablo widgeta.
+ Argument je widget koji sluzi kao wraper za ostale
+ widgete.
  */
 void main() => runApp(MaterialApp(
-  home: Text('Wie geht es'),
+  home: Scaffold(
+    appBar: AppBar(                 // za gornji deo aplikacije
+      title: Text('My first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(                   // u body imamo center widget koji sve sto je u njemu centrira
+      child: Text(
+          'Es tut mir leid',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('push'),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
 ));
+
+/*
+  Scaffold widget nam omogucava osnovni layout za nasu aplikaciju.
+ */
