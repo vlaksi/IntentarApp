b2b6965565ab65bc5d77ab063147169feb16a191
vlaksi <daksrapsody@gmail.com>
2020-02-13 00:33:53 +0100

    Pravljenje custom widgeta & 'HotReload'

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,47 @@ import 'package:flutter/material.dart';
  widgete.
  */
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(                 // za gornji deo aplikacije
-      title: Text('My first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(                   // u body imamo center widget koji sve sto je u njemu centrira
-      child: Text(
-          'Es tut mir leid',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('push'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+    home: PocetnaStrana(),
 ));
 
 /*
   Scaffold widget nam omogucava osnovni layout za nasu aplikaciju.
+
+  Stateless - widget koji se ne moze menjati tokom vremena.
+  Stateful  - widget koji se moze menjati tokom vremena.
+
  */
+
+/*
+    Ovo nam omogucava 'HotReload' kako bi mogli videti rezultate
+    nase izmene u kodu instant.
+ */
+class PocetnaStrana extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(                 // za gornji deo aplikacije
+            title: Text('My first app'),
+            centerTitle: true,
+            backgroundColor: Colors.red[600],
+        ),
+        body: Center(                   // u body imamo center widget koji sve sto je u njemu centrira
+            child: Text(
+                'Ist alles okey ?',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    color: Colors.grey[600],
+                    fontFamily: 'IndieFlower',
+                ),
+            ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Text('push'),
+            backgroundColor: Colors.red[600],
+        ),
+    ) ;
+  }
+}
